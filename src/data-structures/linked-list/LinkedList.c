a3f78aaeab8ae33e3d920652e6a1d7741066c3c2
Gerdt Steltner <gerdt.steltner@gmail.com>
2018-11-22 12:19:14 +0100

    Add LinkedList

---
+++ src/data-structures/linked-list/LinkedList.c
@@ -0,0 +1,36 @@
+typedef struct LinkedListNode {
+  int value;
+  struct LinkedListNode *next;
+} LinkedListNode;
+
+typedef struct LinkedList {
+    struct LinkedListNode *head;
+    struct LinkedListNode *tail;
+} LinkedList;
+
+
+void ll_append(LinkedList linkedList, int value) {
+ if(linkedList->head) {
+  
+ }
+}
+
+void ll_prepend() {
+}
+
+void ll_contains() {
+}
+
+void ll_remove() {
+}
+
+void ll_traverse() {
+}
+
+void ll_reverseTraversal() {
+}
+
+
+int main() {
+
+}
